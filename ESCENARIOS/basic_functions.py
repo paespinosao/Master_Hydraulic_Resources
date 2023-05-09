84be60a935482b11705aa74d41b72faa9246e095
Paula Espinosa <paespinosao@unal.edu.co>
2023-05-08 23:09:52 -0500

    Scripts_v1.1_date_08_05_2023

---
+++ ESCENARIOS/basic_functions.py
@@ -0,0 +1,58 @@
+# Librerias
+import numpy as np
+from pyproj import Proj
+
+
+
+# Funciones
+def proj_ZONE17 (lon, lat):
+    """
+    Función para cambiar las proyecciones de las coordenadas:
+    -----------------------------------------------------------
+    Input:
+    lon, lat
+    ----------------------------------------------------------
+    Output:
+    UTMX, UTMY
+    """
+    myProj = Proj("+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84")
+    UTMx, UTMy = myProj(lon, lat)
+    return UTMx,UTMy
+
+def coord_wgs (UTMx,UTMy):
+    """
+    Función para cambiar georeferenciar las proyecciones:
+    ----------------------------------------------------------
+    Input:
+    UTMX, UTMY
+    ----------------------------------------------------------
+    Output:
+    lon, lat
+    """
+    myProj = Proj("+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84")
+    lon, lat = myProj(UTMx,UTMy, inverse = True)
+    return 360 + lon, lat
+    
+    
+def min_ind (value, vector, delta):
+    """Función para encontrar la mínima diferencia entre dos celdas
+    Input: 
+    - Value: valor a encontrar 
+    - Vector: vectores donde quiere encontrar dicho valor
+    - delta: radio para encontrar ese valor 
+    ----------------------------------------------------------------------
+    Output: 
+    - Ind: indices donde mayor se aproxima el valor buscado a los vectores
+    """
+    indices = np.where((vector > value-(delta)) & (vector < value+(delta)))[0]
+    for i in indices: 
+        try :
+            if vector[i] - value < vector[i+1] - value:
+                ind = i
+            else:
+                ind = i+1
+        except:
+            print('**********ERROR************')
+    
+    print('Indice:',ind,', Valor:', vector[ind], ', Valor real:', value)
+    return ind
